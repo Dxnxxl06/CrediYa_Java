5642cc9bdfa85882cb6e23e80a92400d429e2bc4
Dxnxxl06 <danielplazas1929@gmail.com>
2025-12-18 12:58:54 -0500

    examen

--- src/main/java/com/crediya/servicio/GestorDePagos.java
+++ src/main/java/com/crediya/servicio/GestorDePagos.java
@@ -1,15 +1,39 @@
 package com.crediya.servicio;
 
-import com.crediya.dao.PagoDAO;
-import com.crediya.dao.PrestamoDAO;
-import com.crediya.modelo.Pago;
-import com.crediya.modelo.Prestamo;
-import com.crediya.modelo.enums.EstadoPrestamo;
-import com.crediya.utilidades.ArchivoUtil;
-import java.util.List;
-
+import java.math.BigDecimal;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
 
 public class GestorDePagos {
-    private Pago pago;
+    private final String url = "jdbc:mysql://localhost:3306/crediya_db";
+    private final String usuario = "root";
+    private final String password = "admin";
+    
+    
+    public void registrarPago(int prestamoId, BigDecimal montoPagado, LocalDate fechaPago) 
+        throws prestamoNoEncontradoException {
+            Connection conexion = null;
+            PreparedStatement psVerificar = null;
+            PreparedStatement psInsertarPago = null;
+            PreparedStatement psActualizarPrestamo = null;
+            try {
+                conexion = DriverManager.getConnection(url, usuario, password);
+                String sqlVerficar = """
+                        SELECT saldo_pendiente, estado
+                        FROM prestamos
+                        WHERE id = ?
+                        """;
+                psVerificar = conexion,PreparedStatement(sqlVerficar);
+                psVerificar.setInt(1, prestamoId);
+                rs = psVerificar.executeQuery();
+                } if (rs.next()) {
+                throw new {
+                    prestamoNoEncontradoException("No se encontro un prestamo con ID:" + prestamoId)
+                }
+                BigDecimal
+            }
+    }
     
 }
